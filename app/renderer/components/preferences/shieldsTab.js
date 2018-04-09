ab33deb9c617a4b6deffe5bb8f1e43f4c2fdf500
Pranjal <pranjal@brave.com>
2018-04-09 10:49:01 -0700

    persist safe browsing control in preferences

--- app/renderer/components/preferences/shieldsTab.js
+++ app/renderer/components/preferences/shieldsTab.js
@@ -46,7 +46,6 @@ class ShieldsTab extends ImmutableComponent {
   constructor (props) {
     super(props)
     this.onChangeAdControl = this.onChangeAdControl.bind(this)
-    this.onChangeSafeBrowsingControl = this.onChangeSafeBrowsingControl.bind(this)
     this.onToggleHTTPSE = this.onToggleSetting.bind(this, httpsEverywhere)
     this.onToggleNoScript = this.onToggleSetting.bind(this, noScript)
   }
